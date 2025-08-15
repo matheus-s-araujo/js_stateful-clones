a1bc9e9d29f8b2be85df75da3b2ff3be7d25e56f
Matheus Sousa <theus.sousaa@gmail.com>
2025-08-15 20:42:08 -0300

    finished project

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state }; // cópia inicial
+  const statesArray = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+    statesArray.push({ ...currentState }); // sempre push de uma cópia!
+  }
+
+  return statesArray;
 }
 
 module.exports = transformStateWithClones;
